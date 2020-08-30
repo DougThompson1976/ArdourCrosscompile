16cb0bdb50199def67a3e852f0e342d7c54cc1af
Tremeschin <tremeschin@protonmail.com>
2020-08-30 03:40:45 -0400

    Really basic stuff,  REAMDE

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# ardour-crosscompile-arch
-Script to cross compile Ardour DAW to Windows from Arch Linux
+# Python script for cross compiling Ardour DAW from Arch Linux targeting Windows
+
+WIP, lots of steps to cross compile Ardour.
+
+This script will work best on a Arch Linux distribution, it can work on other based distros like Manjaro, however I offer no support for those.
+
+#### Why Arch?
+
+Out of Ubuntu, Fedora, OpenSUSE and Arch Linux, I was only successful cross compiling on Arch.
+
+If you're on Windows, consider running this on WSL (Windows Subsystem for Linux), but you really should just use Linux, we have many free (as in price and freedom) plugins, such as Calf Studio Gear, ZynFusion, Disthro ports of LV2 / VST, TAL Plugins, and many others honorable mentions.
+
+Compiling Ardour for Windows IS hard, under the penguin it is a lot more simple and straightforward.
+
+# Roadmap
+
+- [ ] Basic shell functions (rmdir, sed a file, mkdir, etc)
+
+- [ ] Download sources, urls simple way
+
+- [ ] Manually patch files (mainly two PKGBUILDS)
+
+- [ ] Pacman wrapper for installing dependencies, needed ones, etc.
+ 
+- [ ] From a finished build, package ardour
+
+And others I might add as development goes on.
+
+# Legal, disclaimer
+
+Only the files that comes with this repo are under the MIT license, downloaded files and sources licenses have their own license, own creators, etc.
+
+If you find something wrong (like me downloading a file the user should do by hand because distribution problems), let me know!!
+
+### Ardour developers offer NO SUPPORT and WARRANTY on DIY builds and third party.
+
+## CONSIDER SUPPORTING ARDOUR DEVELOPMENT, BUY THEIR PRODUCT OR A SUBSCRIPTION.
---
+++ main.py
@@ -0,0 +1,118 @@
+"""
+MIT License
+
+Copyright (c) 2020 Tremeschin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import neotermcolor
+import shutil
+import sys
+import os
+
+cprint = neotermcolor.cprint
+
+# Set neotermcolor styles
+neotermcolor.set_style('e', color='red',    attrs='bold') # Error
+neotermcolor.set_style('w', color='yellow', attrs='bold') # Warning
+neotermcolor.set_style('i', color='white',  attrs='bold') # Info
+neotermcolor.set_style('q', color='yellow', attrs='bold') # Question
+neotermcolor.set_style('c', color='blue',   attrs='bold') # Check
+neotermcolor.set_style('o', color='green',  attrs='bold') # Ok
+
+# Intents for printing pretty stuff
+class Indents:
+    ERROR =    "## ERROR: text"
+    WARNING =  "[WARNING] :: text"
+    INFO =     "[INFO] text"
+    QUESTION = " > text:"
+    CHECK =    "[CHECKING] ( text )"
+    OK =       "[OK] - text"
+
+
+# Utilities class
+class Utils:
+    def __init__(self, main):
+        self.main = main
+        self.ROOT = os.path.dirname(os.path.abspath(__file__))
+    
+    # (S)tyled (print) with indents
+    def sprint(self, text, style="info"):
+        # Defaults and indents
+        indent = {
+            "e": Indents.ERROR,
+            "w": Indents.WARNING,
+            "q": Indents.QUESTION,
+            "c": Indents.CHECK,
+            "o": Indents.OK,
+        }.get(style[0], Indents.INFO)
+        
+        cprint(indent.replace("text", text), style=style)
+
+    # Make directory if doesn't exist
+    def mkdir_dne(self, path, check=True):
+        
+        # Warn "checking" for the user
+        self.sprint(f"MKDIR DNE [{path}]", 'c')
+
+        # Does directory already exist?
+        if not os.path.exists(path):
+            self.sprint(f"Directory doesn't exist, creating", 'w')
+            os.makedirs(path)
+            if check:
+                if not os.path.exists(path):
+                    self.sprint(f"Could NOT create directory [{path}]", 'e')
+                    sys.exit(-1)
+        else:
+            # Path already exist
+            self.sprint("Path already exists", 'o')
+    
+    # Delete a directory and its contents
+    def rmdir(self, path):
+
+        # Warn "checking" for the user
+        self.sprint(f"RMDIR [{path}]", 'c')
+        
+        if os.path.isdir(path):
+
+            # Delete the directory
+            shutil.rmtree(path, ignore_errors=True)
+            
+            # Directory still exists? Ew, error
+            if os.path.isdir(path):
+                self.sprint(f"Tried to remove directory [{path}] but still exists", 'e')
+                sys.exit(-1)
+        else:
+            self.sprint("Directory doesn't exist, skipping...", 'o')
+
+
+class Directories:
+    def __init__(self, main):
+        self.main = main
+
+
+class Main:
+    def __init__(self):
+        self.utils = Utils(self)
+        self.directories = Directories(self)
+        self.utils.mkdir_dne(self.utils.ROOT + "ardour")
+
+Main()
+# print(os.system("ls -l3"))

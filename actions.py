881dbcb94dee194438de84bdaf55bb866d68e36e
Tremeschin <tremeschin@protonmail.com>
2020-09-03 03:33:06 -0400

    Actually, the env is for the run() method

--- actions.py
+++ actions.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 
 from config import *
+import os
 
 class Actions:
     def __init__(self, main):
@@ -151,7 +152,5 @@ class Actions:
             sub.from_string((
                 "/usr/bin/bash"
                 f" \"{self.main.directories.ardour}tools/x-win/compile.sh\""
-                ),
-                env = env
-            )
-            sub.run(shell=True)
+            ))
+            sub.run(env = env, shell=True)
